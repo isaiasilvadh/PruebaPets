4720d56d547b96b1f53848606635ee3fd28474d9
isaiasilvadh <isaiasilva64@gmail.com>
2022-11-10 17:54:43 -0500

    prueba terminada

--- app/src/main/java/com/example/pets_servicios/Res_Imc.kt
+++ app/src/main/java/com/example/pets_servicios/Res_Imc.kt
@@ -4,15 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_imc.*
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_res_imc.*
 
-class Imc : AppCompatActivity() {
+class Res_Imc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_imc)
+        setContentView(R.layout.activity_res_imc)
 
-        btn_calc.setOnClickListener {
-            val saltar: Intent = Intent(this, Res_Imc::class.java)
+        btn_regresar.setOnClickListener {
+            val saltar: Intent = Intent(this, MainActivity::class.java)
             startActivity(saltar)
         }
     }
